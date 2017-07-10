368c7ab61d7432be2ea6d6a62459b08884936b5c
Bruno Ortiz <brunoorla@gmail.com>
2017-07-10 09:41:13 +0200

    Added Player and PlayerRepository classes
    Created and tested the database system using a Player class and its PlayerRepository class (extends JpaRepository)

--- src/main/java/com/AGI/salvo/SalvoApplication.java
+++ src/main/java/com/AGI/salvo/SalvoApplication.java
@@ -1,12 +1,25 @@
 package com.AGI.salvo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SalvoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalvoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(PlayerRepository playerRepository) {
+		return args -> {
+			System.out.println("RUNNING THE COMMAND LINE RUNNER");
+			playerRepository.save(new Player("j.bauer@ctu.gov"));
+			playerRepository.save(new Player("c.obrian@ctu.gov"));
+			playerRepository.save(new Player("kim_bauer@gmail.com"));
+			playerRepository.save(new Player("t.almeida@ctu.gov"));
+		};
 	}
 }
